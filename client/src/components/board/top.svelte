ce54a0576e480ce83854abe222ed52e30ff565fa
trixky <mabois@student.42.fr>
2022-10-21 18:04:31 +0200

    [Dev] player infos: add player infos

---
+++ client/src/components/board/top.svelte
@@ -0,0 +1,92 @@
+<!-- ========================= SCRIPT -->
+<script lang="ts">
+
+export let mode_ia: boolean;
+export let turn: 1 | 2;
+
+let player_1 = {
+    pieces_nbr: 0,
+    max: 0,
+}
+
+let player_2 = {
+    pieces_nbr: 0,
+    max: 0,
+}
+
+</script>
+
+<!-- ========================= HTML -->
+<div class="top-container">
+    <div class="player-container left">
+        <h2>
+        <span class:turn={turn === 1}>
+            Player 1
+        </span>
+            {#if mode_ia}
+            <span class="opacity-20">
+                (You)
+            </span>
+            {/if}
+        </h2>
+        <div class="player-infos left">
+            <p>pieces: {player_1.pieces_nbr}</p>
+            <p>max: {player_1.max}</p>
+        </div>
+    </div>
+    <div class="player-container right">
+        <h2>
+            <span class:turn={turn === 2}>
+                Player 2
+            </span>
+            {#if mode_ia}
+            <span class="opacity-20">
+                (AI)
+            </span>
+            {/if}
+        </h2>
+        <div class="player-infos right">
+            <p>pieces: {player_2.pieces_nbr}</p>
+            <p>max: {player_2.max}</p>
+        </div>
+    </div>
+</div>
+
+<!-- ========================= CSS -->
+<style lang="postcss">
+    .top-container {
+        @apply flex justify-between mb-2;
+    }
+
+    .player-container {
+        @apply p-1;
+    }
+
+    .player-infos {
+        @apply flex pt-[9px];
+    }
+
+    .player-container.left > h2 {
+        @apply text-left;
+    }
+
+    .player-container.right > h2 {
+        @apply text-right;
+    }
+
+    .player-container.left > .player-infos > p {
+        @apply mr-2;
+    }
+
+    .player-container.right > .player-infos > p {
+        @apply ml-2;
+    }
+
+    .player-infos > p {
+        @apply flex text-sm;
+    }
+
+    .turn {
+        @apply underline underline-offset-2;
+    }
+</style>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <!-- ========================= SCRIPT -->
 <script lang="ts">
 	import Board from '../components/board/board.svelte';
+	import TopBoard from '../components/board/top.svelte';
+
+    let mode_ia = true;
+    let turn: 1 | 2 = 1;
 </script>
 
 <!-- ========================= HTML -->
 
-<Board />
+<div class="game">
+    <TopBoard mode_ia={mode_ia} turn={turn}/>
+    <Board />
+</div>
