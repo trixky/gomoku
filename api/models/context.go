408bd16080e6b86f1901322408d81c9e0ab88d10
trixky <mabois@student.42.fr>
2022-10-29 15:22:45 +0200

    [Clean] negamax: remove the old negamax version

--- api/models/context.go
+++ api/models/context.go
@@ -36,49 +36,6 @@ func (c *Context) Next(position Position) (context Context) {
 	return
 }
 
-// OldNegamax ...
-func (c *Context) OldNegamax() {
-	// https://en.wikipedia.org/wiki/Negamax
-
-	if c.State.Depth < c.Options.DepthMax {
-		// value := −∞
-		var value int32 = math.MinInt32
-
-		for y, line := range c.Goban {
-			// For each line
-			for x, cell := range line {
-				// For each cell
-				if cell >= c.Options.ProximityThreshold {
-					// If the cell is in the
-					// @@@ foreach child in childNodes do
-					child_context := c.Next(Position{
-						X: uint8(x),
-						Y: uint8(y),
-					})
-
-					child_context.OldNegamax()
-
-					// @@@ value := max(value, −negamax(child, depth − 1, −β, −α, −color))
-
-					if child_context.State.Alpha > value {
-						value = -child_context.State.Alpha
-					}
-					// @@@ α := max(α, value)
-					if c.State.Alpha < value {
-						c.State.Alpha = value
-					}
-					// @@@ if α ≥ β then
-					if c.State.Alpha >= c.State.Beta {
-						return
-					}
-				}
-			}
-		}
-	} else {
-		c.State.Alpha = rand.Int31n(100)
-	}
-}
-
 // Negamax ...
 func (c *Context) Negamax() *Context {
 	if c.State.Depth < c.Options.DepthMax {
