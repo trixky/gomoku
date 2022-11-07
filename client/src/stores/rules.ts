f00907aacaabefc995f1aac417fe688989125bf2
trixky <mabois@student.42.fr>
2022-11-07 10:24:29 +0100

    [Dev] rules: add rules display option to the front

---
+++ client/src/stores/rules.ts
@@ -0,0 +1,8 @@
+export default [
+	'Two players take turns placing stones of their color on an intersection of the board.',
+	'The game ends when one player manages to <b>captures ten opposing stones</b> or <b>align five stones or more</b>.',
+	'A player that manages to align five stones <b>only wins if</b> the opponent can not break this alignment by capturing a pair, or if he has already lost four pairs and the opponent can capture one more, therefore winning by capture.',
+	'You can <b>capture</b> a pair of your opponent’s stones to remove it from the board by <b>flanking</b> them with your own stones.',
+	'It is forbidden to play a move that introduces two <b>free-three alignments</b>, which would guarantee a win by alignment.',
+	'<b>No limit</b> to the number of stones.'
+];
