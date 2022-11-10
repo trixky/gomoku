2ffe519c21a25d5bed651b908764c0dc4aa03fb1
Mathis Bois <mathis.bois@outlook.fr>
2022-11-10 16:14:37 +0100

    [Dev] check: call the check endpoint for test

--- client/src/api/post.check.ts
+++ client/src/api/post.check.ts
@@ -2,19 +2,23 @@ import Config from '../config';
 import type { AlgoOptions as AlgoOptionsModel } from '../models/algo_options';
 import type { PlayersInfo as PlayersInfoModel } from '../models/players_info';
 
-function postNext(
+function postCheck(
 	goban: string,
 	options: AlgoOptionsModel,
 	players_info: PlayersInfoModel
 ): Promise<string> {
 	return new Promise<string>((resolve, reject) => {
-		const url = `http://${Config.environment.api.origin}:${Config.environment.api.port}/next`;
+		const url = `http://${Config.environment.api.origin}:${Config.environment.api.port}/check`;
+
+		console.log('ce qui va partir pour le check:');
 
 		const data = {
 			options,
 			goban,
 			players_info
 		};
+
+		console.log(data);
 
 		fetch(url, {
 			method: 'POST',
@@ -36,4 +40,4 @@ function postNext(
 	});
 }
 
-export default postNext;
+export default postCheck;
