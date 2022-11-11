60942347e1d52f4d28c21cb19f0cec95694d4851
thervieu <thibault99hervieu@gmail.com>
2022-11-11 09:48:23 +0100

    [Dev]: api: no more win check in backend

--- api/server/next.go
+++ api/server/next.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/trixky/gomoku/doubleThree"
 	"github.com/trixky/gomoku/logic"
 	"github.com/trixky/gomoku/models"
 )
@@ -79,8 +78,6 @@ choose:
 	}
 
 	elapsed_time := time.Now().Sub(context.Start).Milliseconds()
-
-	doubleThree.IsWin(&best_child)
 
 	_json, err := best_child.ToJSON(elapsed_time, heuristic_goban)
 
