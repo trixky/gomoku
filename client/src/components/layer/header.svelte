869f42347ae068aa11bcd9b9e3f9430b6b2a5d60
trixky <mabois@student.42.fr>
2022-11-13 19:51:20 +0100

    [Fix] header title: reduce the width of the a balise to the github link on the header title

--- client/src/components/layer/header.svelte
+++ client/src/components/layer/header.svelte
@@ -1,11 +1,17 @@
 <!-- ========================= HTML -->
 <header>
-	<a href="https://github.com/trixky/gomoku" target="_blank" rel="noreferrer"><h1>Gomoku</h1></a>
+	<h1>
+		<a href="https://github.com/trixky/gomoku" target="_blank" rel="noreferrer">Gomoku</a>
+	</h1>
 </header>
 
 <!-- ========================= CSS -->
 <style lang="postcss">
+	h1 {
+		@apply w-fit m-auto px-2;
+	}
+
 	header {
-		@apply p-8 text-center mb-3;
+		@apply p-8 mb-3;
 	}
 </style>
