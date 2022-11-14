398f24dba24dfe223c91a6fa3e35479bdee87d59
thervieu <thibault99hervieu@gmail.com>
2022-11-14 20:16:36 +0100

    [Fix]: api: heuristics: one file per heuristic, random is back

--- api/heuristics/capture.go
+++ api/heuristics/capture.go
@@ -1,16 +1,34 @@
 package heuristics
 
 import (
-	"math/rand"
-
 	"github.com/trixky/gomoku/models"
 )
 
 // Capture computes the capture heuristic of a given context
-func Capture(context *models.Context) int {
-	if context.Options.HeuristicRandomWeight > 0 {
-		// If the weight of the heuristic is not null
-		return (int(rand.Int31n(101)) - 50) * context.Options.HeuristicCaptureWeight
+func Capture(context *models.Context, player uint8) int {
+	if context.Options.HeuristicCaptureWeight >= 0 {
+		var score int
+
+		capturesP1 := context.State.PlayersInfo.Player_1.Captures
+		capturesP2 := context.State.PlayersInfo.Player_2.Captures
+		if player == 255 {
+			capturesP1 = context.State.PlayersInfo.Player_2.Captures
+			capturesP2 = context.State.PlayersInfo.Player_1.Captures
+		}
+
+		if capturesP1 >= 10 {
+			score += context.Options.HeuristicCaptureWeight * 20000
+		} else if capturesP2 >= 10 {
+			score -= context.Options.HeuristicCaptureWeight * 20000
+		} else if capturesP1 == 8 {
+			score += context.Options.HeuristicCaptureWeight * 5000
+		} else if capturesP2 == 8 {
+			score -= context.Options.HeuristicCaptureWeight * 5000
+		} else {
+			score += context.Options.HeuristicCaptureWeight * 2000 * (int(capturesP1) - int(capturesP2))
+		}
+
+		return score
 	}
 
 	return 0
