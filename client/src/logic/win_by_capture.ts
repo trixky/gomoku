2c38b5bc5e170163ba4c5110615a8ca1d76b087a
trixky <mabois@student.42.fr>
2022-11-14 20:12:59 +0100

    [Fix] capture: winner by capture was inversed

--- client/src/logic/win_by_capture.ts
+++ client/src/logic/win_by_capture.ts
@@ -3,9 +3,9 @@ import type { PlayersInfo as PlayersInfoModel } from '../models/players_info';
 export default function winByCapture(players_info: PlayersInfoModel): 0 | 1 | 2 {
 	let winner: 0 | 1 | 2 = 0;
 
-	if (players_info.player_2.captures >= 10) {
+	if (players_info.player_1.captures >= 10) {
 		winner = 1;
-	} else if (players_info.player_1.captures >= 10) {
+	} else if (players_info.player_2.captures >= 10) {
 		winner = 2;
 	}
 
